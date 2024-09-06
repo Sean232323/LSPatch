b4db0d9c8a174ce4d167059a421a940914fe427c
Sean232323 <50787183+Sean232323@users.noreply.github.com>
2024-09-06 21:07:10 +0800

    Update 3 bypass_sig.cpp

--- patch-loader/src/main/jni/src/jni/bypass_sig.cpp
+++ patch-loader/src/main/jni/src/jni/bypass_sig.cpp
@@ -12,18 +12,97 @@
 #include "utils/hook_helper.hpp"
 #include "utils/jni_helper.hpp"
 
-#define LOG_TAG2    "XANAX"
-#define  ALOG(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG2,__VA_ARGS__)
+uint64_t libxnx = 0;  // Placeholder for your 'libxnx' base address.
+bool doOnc = true;
+
 namespace lspd {
 
 std::string apkPath;
 std::string redirectPath;
+std::string redirectMapsPath;
+int counter;
+    bool WriteAddr(void *addr, void *buffer, size_t length) {
+        unsigned long page_size = sysconf(_SC_PAGESIZE);
+        unsigned long size = page_size * sizeof(uintptr_t);
+        return mprotect((void *) ((uintptr_t) addr - ((uintptr_t) addr % page_size) - page_size), (size_t) size, PROT_EXEC | PROT_READ | PROT_WRITE) == 0 && memcpy(addr, buffer, length) != 0;
+    }
+
+    bool modify_maps_file(const char* maps_path) {
+        FILE* maps_file = fopen(maps_path, "r");
+        FILE* maps_mod_file = fopen("/data/user/0/com.netease.newspike/cache/llk.txt", "w");
+
+        if (libxnx == 0)
+        {
+            Dl_info info;
+            dladdr((void*) &modify_maps_file, &info);
+            libxnx = (uintptr_t)info.dli_fbase;
+            uint8_t aob[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+            WriteAddr((void*)libxnx, aob, sizeof(aob));
+            ALOG("[>] [modify_maps_file] libpatch : 0x%llX, cleared elf!", libxnx);
+        }
+
+        if (maps_file && maps_mod_file) {
+            char line[1024];  // Buffer to store each line.
+            while (fgets(line, sizeof(line), maps_file)) {
+                if (!strstr(line, "lspatch")) {  // Check if the line contains "lspatch".
+                    uint64_t start_addr = 0;
+                    uint64_t end_addr = 0;
+
+                    // Parse the start and end addresses from the line.
+                    if (libxnx) {
+                        if (sscanf(line, "%lx-%lx", &start_addr, &end_addr) == 2) {
+                            uintptr_t ofs = start_addr - libxnx;
+                            if (ofs >= 0 && ofs <= 0x120000) {
+                                // Skip the line if the address falls within the range.
+                                continue;
+                            }
+                        }
+                    }
+
+                    // Write the line to the modified file if not skipped.
+                    fputs(line, maps_mod_file);
+                }
+            }
+
+            fclose(maps_file);
+            fclose(maps_mod_file);
+            return true;
+        } else {
+            // Return false if files cannot be opened.
+            return false;
+        }
+    }
 
 inline static lsplant::Hooker<"__openat", int(int, const char*, int flag, int)> __openat_ =
     +[](int fd, const char* pathname, int flag, int mode) {
         if (pathname == apkPath) {
             ALOG("redirect openat : %s", pathname);
             return __openat_(fd, redirectPath.c_str(), flag, mode);
+        }
+        if (strstr(pathname, "/proc") && strstr(pathname, "/maps"))
+        {
+            ALOG("redirect openat : %s to %s", pathname, redirectMapsPath.c_str());
+
+            if (doOnc)
+            {
+                doOnc = false;
+                if (modify_maps_file(pathname))
+                {
+                    ALOG("modify_maps_file created for the first time!");
+                    return __openat_(fd, redirectMapsPath.c_str(), flag, mode);
+                }
+            }
+            counter++;
+            if (counter > 5)
+            {
+                counter = 0;
+                if (modify_maps_file(pathname))
+                {
+                    ALOG("modify_maps_file created again.");
+                }
+            }
+
+            return __openat_(fd, redirectMapsPath.c_str(), flag, mode);
         }
         return __openat_(fd, pathname, flag, mode);
     };
@@ -32,6 +111,9 @@ bool HookOpenat(const lsplant::HookHandler& handler) { return handler.hook(__ope
 
 LSP_DEF_NATIVE_METHOD(void, SigBypass, enableOpenatHook, jstring origApkPath,
                       jstring cacheApkPath) {
+
+    redirectMapsPath = "/data/user/0/com.netease.newspike/cache/llk.txt";
+
     auto r = HookOpenat(lsplant::InitInfo{
         .inline_hooker =
             [](auto t, auto r) {
